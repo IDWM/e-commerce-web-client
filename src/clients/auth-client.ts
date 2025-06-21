77d4297b7ff18b23f58e419940730a360c7a8768
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2025-06-20 20:00:04 -0400

    feat: create auth client

---
+++ src/clients/auth-client.ts
@@ -0,0 +1,10 @@
+import { apiClient } from './axios-client';
+
+import type { LoginRequest, LoginResponse } from '@/models';
+
+export const authClient = {
+  login: async (credentials: LoginRequest): Promise<LoginResponse> => {
+    const response = await apiClient.post<LoginResponse>('/auth/login', credentials);
+    return response.data;
+  },
+};
