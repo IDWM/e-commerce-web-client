f74a9c3ce9c9a676becb41a4a3d5356fbc11f05f
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2025-06-21 20:51:34 -0400

    feat: configure image domains

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['res.cloudinary.com', 'cdn-icons-png.flaticon.com'],
+  },
 };
 
 export default nextConfig;
